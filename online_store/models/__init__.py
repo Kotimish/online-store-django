5af9bf288359c9b08bca83d4ab04e347cc358b35
Kotimish <kotov.mikhail.96@gmail.com>
2026-01-12 17:55:04 +0300

    add timestamp mixin and product model

---
+++ online_store/models/__init__.py
@@ -0,0 +1,5 @@
+__all__ = {
+    'Product'
+}
+
+from online_store.models.product import Product
---
+++ online_store/models/product.py
@@ -0,0 +1,14 @@
+from django.db import models
+from online_store.models.mixins import TimestampMixin
+
+class Product(TimestampMixin):
+    name = models.CharField(
+        max_length=100
+    )
+    description = models.TextField()
+    price = models.DecimalField(
+        max_digits=12,
+        decimal_places=2,
+        null=True,
+        blank=True
+    )
