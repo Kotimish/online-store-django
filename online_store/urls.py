78e85ef62a620fc4b48b090b59ccc56da0e0c338
Kotimish <kotov.mikhail.96@gmail.com>
2026-01-12 20:58:16 +0300

    add base templates and views

--- online_store/urls.py
+++ online_store/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from online_store import views
 
 urlpatterns = [
-    path('', views.index),
-    path('about/', views.about),
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('categories/', views.categories_page, name='categories_page'),
+    path('products/', views.products_page, name='products_page'),
+    path('categories/<int:category_id>/products', views.products_in_category_page, name='products_in_category_page'),
+    path('products/<int:product_id>/', views.product_detail, name='product_detail'),
 ]
